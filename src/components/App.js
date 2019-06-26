691d1c4fb1424dfb4d2f8747bceb50bed177684c
thomas-cruz <tom.ortega47@gmail.com>
2019-06-26 19:32:33 +0800

    Added textarea for new cards and lists

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import List from './List';
 import { connect } from 'react-redux';
+import ActionButton from './ActionButton';
 
 class App extends Component {
     render(){
@@ -10,8 +11,9 @@ class App extends Component {
                 <h2>Hello World!</h2>
                 <div style = {styles.listContainer}>
                     {lists.map(list => (
-                        <List title = {list.title} cards = {list.cards}/>
+                        <List key = {list.id} title = {list.title} cards = {list.cards}/>
                     ))}
+                    <ActionButton list/>
                 </div>
             </div>
             );
--- src/components/List.js
+++ src/components/List.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import Card from './Card';
+import ActionButton from './ActionButton';
+
 
 const List = ({title, cards}) => {
     return(
         <div style={styles.container}>
             <h4>{title}</h4>
             {cards.map(card => 
-                (<Card text = { card.text }/>
+                (<Card key = {card.id} text = {card.text}/>
             ))}
+            <ActionButton/>
         </div>
     )
 }
@@ -16,6 +19,7 @@ const styles = {
     container: {
         backgroundColor: "#ccc",
         borderRadius: 3,
+        height: '100%',
         width: 300,
         padding: 8,
         marginRight: 8
